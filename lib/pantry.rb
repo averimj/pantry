cf5190fbca81567de655769c88ddd7139a3f09e3
Averi Johnson <averimj@live.com>
2018-07-25 15:05:21 -0600

    initial commit

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,23 @@
 class Pantry
+  attr_reader :stock,
+              :stock_check
+
+
+
+  def initialize
+    @stock = {}
+    @stock_check = 0
+  end
+
+  def stock_check(item)
+    @stock.keys
+    #hard_coded...I think
+  end
+
+  def restock(item, quantity)
+    @stock[item] = quantity
+  end
+
+
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,34 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exist
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_has_stock
+    pantry = Pantry.new
+
+    assert_equal Hash.new, pantry.stock
+  end
+
+  def test_stock_starts_at_zero
+    pantry = Pantry.new
+
+    pantry.stock_check("Cheese")
+
+    assert_equal  0, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock
+    pantry = Pantry.new
+
+    pantry.restock("Cheese", 10)
+    pantry.stock_check("Cheese")
+
+    assert_equal 10, pantry.restock("Cheese", 10)
+  end
+
+
 end
